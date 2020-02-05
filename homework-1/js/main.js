ca0c1dd1b97ac1952870547080e60fbb5dacaf03
Vadim <polish4uck@gmail.com>
2020-02-05 18:21:09 +0200

    changes HW1 - done

--- homework-1/js/main.js
+++ homework-1/js/main.js
@@ -1,20 +1,21 @@
 const CURRENCY = `₴`;
-let price = {
+const PRICE = {
     apple: 15.678,
     banana: 123.965,
     pineapple: 90.2345
 };
-let {apple, banana, pineapple} = price;
+const {apple, banana, pineapple} = PRICE;
 let sum = apple + banana + pineapple;
-let sumRounded = Math.floor(apple) + Math.floor(banana) + Math.floor(pineapple);
+// let sumRounded = Math.floor(apple) + Math.floor(banana) + Math.floor(pineapple);
+// <p>Сума всіх товарів ${sumRounded}${CURRENCY} (Заокруглення в меншу сторону)</p>
 let sumMedium = sum/3;
 let randomValue = Math.random();
 document.writeln(`
     <p>Максимальна ціна ${Math.max(apple, banana, pineapple)}${CURRENCY}</p>
     <p>Мінімальна ціна ${Math.min(apple, banana, pineapple)}${CURRENCY}</p>
     <p>Сума всіх товарів ${sum}${CURRENCY}</p>
-    <p>Сума всіх товарів ${sumRounded}${CURRENCY} (Заокруглення в меншу сторону)</p>
-    <p>Сума всіх товарів ${Math.round(sum/100)*100}${CURRENCY} (Заокруглення до сотень)</p>
+    <p>Сума всіх товарів ${Math.floor(sum)}${CURRENCY} (Заокруглення в меншу сторону)</p>
+    <p>Сума всіх товарів ${Math.ceil(sum/100)*100}${CURRENCY} (Заокруглення до сотень)</p>
     <p>Чи парна сума (${Math.floor(sum)}) - "${Math.floor(sum)/2 == 0}"</p>
     <p>Решта з 500${CURRENCY} = ${(500 - sum)}${CURRENCY}</p>
     <p>Середнє значення ${+sumMedium.toFixed(2)}${CURRENCY} (Заокруглення до другого знака після коми)</p>
