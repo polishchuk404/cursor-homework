5bbdb577a8bdf0b5e4192832bb85b343997142e8
Vadim <polish4uck@gmail.com>
2020-02-07 17:51:40 +0200

    add hw3

--- homework-2/js/main.js
+++ homework-2/js/main.js
@@ -1,12 +1,11 @@
 let N = null;
 do {
-  N = Math.round(+prompt(`Enter the number "N"`, ``));
-} while (isNaN(N) || N == 0);
+  N = +prompt(`Enter the number "N"`, ``);
+} while (isNaN(N) || N == 0 || N % 1 !== 0 );
 let M = null;
 do {
-    M = Math.round(+prompt(`Enter the number "M"`, ``));
-} while (isNaN(M) || M <= N){
-}
+  M = +prompt(`Enter the number "M"`, ``);
+} while (isNaN(M) || M <= N || M % 1 !== 0)
 let skipEven = confirm("Skip even numbers?");
 let sum = 0;
 for (let i = N; i <= M; i++){
