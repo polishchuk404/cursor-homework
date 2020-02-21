460b01eb3abfbbffb73478f5d4f81c719b811eb1
Vadim <polish4uck@gmail.com>
2020-02-22 00:36:15 +0200

    hw7 add

--- homework-5/js/main.js
+++ homework-5/js/main.js
@@ -9,6 +9,22 @@ function getRandomArray(length, min, max){
     return randomArr;
 }
 
+// 2
+function getModa(numbers){
+    let result = [];
+    number = numbers.sort( (a, b) => a - b );
+    for(let i = 0; i < number.length; i++) {
+        if((numbers[i] ^ 0) === numbers[i]) {
+            if(number[i] === number[++i]) {
+                if(number[i] === number[++i]) {
+                    result.push(number[i]);
+                }
+            }
+        }
+    }
+    return result;
+}
+
 // 3
 function getAverage(numbers){
     result = numbers.filter(value => (parseFloat(value) - parseInt(value) === 0));
@@ -42,13 +58,22 @@ function countPositiveNumbers(numbers){
 function getDividedByFive(numbers){
     return numbers.filter(something => something % 5 === 0).join(", ");
 }
+
+// 8
+const notCensorshipText = `"Are you fucking kidding?", "Holy shit!", "It's bullshit!"`;
+function replaceBadWords(string){
+    return string.replace(/(fuck|shit)/g, "****");
+}
+
 document.writeln(`  
 <p>Функція №1: (${getRandomArray(15, 1, 100).join(", ")})</p>
+<p>Функція №2: (${getModa(arrNambers)})</p>
 <p>Функція №3: (${getAverage(arrNambers)})</p>
 <p>Функція №4: (${getMedian(arrNambers)})</p>
 <p>Функція №5: (${filterEvenNumbers(arrNambers)})</p>
 <p>Функція №6: (${countPositiveNumbers(arrNambers)})</p>
 <p>Функція №7: (${getDividedByFive(arrNambers)})</p>
+<p>Функція №8: (${replaceBadWords(notCensorshipText)})</p>
                   `);
 
                   
