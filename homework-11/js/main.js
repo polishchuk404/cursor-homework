b52ed5427a9781116e8c00c7da0d53f959782675
Vadim <polish4uck@gmail.com>
2020-03-07 15:00:13 +0200

    hw11 updated

--- homework-11/js/main.js
+++ homework-11/js/main.js
@@ -1,16 +1,19 @@
+const delay = ms => {
+  return new Promise(resolve => setTimeout(() => resolve(), ms));
+}
 const getSymbol = () => {
   return new Promise((resolve) => {
-    setTimeout(()=> {
       const sign = `${Date.now()}`.slice(-5);
       resolve(String.fromCharCode(sign));
-  }, 50);
   });
 }
-async function getRandomChinese(length){
+
+async function getRandomChinese(length, time){
   let str = [];
   while(str.length < length){
     str.push(await getSymbol());
+    await delay(time);
   } 
   return console.log(str.join(""));   
 } 
-getRandomChinese(4).then();
+getRandomChinese(4, 50);
